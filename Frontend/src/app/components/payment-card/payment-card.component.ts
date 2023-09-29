9b269ae77dd5953a91150b96a2a733f3c72c85bd
VVeljovic <veljkoveljovic13@gmail.com>
2023-09-30 01:33:24 +0200

    late work

--- Frontend/src/app/components/payment-card/payment-card.component.ts
+++ Frontend/src/app/components/payment-card/payment-card.component.ts
@@ -1,27 +1,30 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { Stripe, StripeCardElement, loadStripe } from '@stripe/stripe-js';
-import { STRIPE_PUBLISH_KEY } from 'src/environments/environment';
-import { PaymentService } from 'src/app/services/payment.service';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
-import { tap } from 'rxjs';
+
+import { Observable, tap } from 'rxjs';
+import { NotificationPopupComponent } from '../notification-popup/notification-popup.component';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user.model';
+
 
 @Component({
   selector: 'app-payment-card',
   templateUrl: './payment-card.component.html',
   styleUrls: ['./payment-card.component.scss']
 })
-export class PaymentCardComponent {
-  private stripePromise: Promise<Stripe | null>;
+export class PaymentCardComponent{
+ user$:Observable<User|null>|undefined;
   cardNumber: string = '';
   yearNumber : string = ''
   mounthNumber : string = ''
   cvc: string = '';
 
 
-  constructor(private service: PaymentService) {
-    this.stripePromise = loadStripe(STRIPE_PUBLISH_KEY);
+  constructor(private dialogRef:MatDialog,private userService:UserService) {
+    
   }
-
+ 
   onCardNumberInput() {
     this.cardNumber = this.cardNumber.replace(/\s/g, '');
    // this.cardNumber = this.cardNumber.replace(/(\d{4})/g, '$1 ');
@@ -37,40 +40,56 @@ export class PaymentCardComponent {
     this.mounthNumber = this.mounthNumber.replace(/\s/g, '');
   }
   onSubmit() {
-    console.log(this.cardNumber + this.cvc);
-    let response = '';
-    this.service.checkout().pipe(
-      tap(resp => {
-        response = resp.clientSecret;
-        console.log(resp);
-      })
-    ).subscribe(() => {
-      console.log(response);
-      this.initializeStripe(response);
-    });
-  }
-
-  async initializeStripe(clientSecret: string) {
-    console.log(clientSecret);
-    const stripe = await this.stripePromise;
-    if (stripe) {
-
-      const elements = stripe.elements();
-      const cardElement = elements.create('card');
-    
-      cardElement.mount('#card-element');
-     
-      const { paymentIntent, error } = await stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-          card: cardElement,
-        },
-      });
-
-      if (error) {
-        console.error('Greška prilikom procesiranja plaćanja:', error);
-      } else if (paymentIntent.status === 'succeeded') {
-        console.log('Plaćanje je uspešno.');
-      }
+    // Uklonite sve razmake iz brojeva kartice i CVC-a
+    const cleanedCardNumber = this.cardNumber.replace(/\s/g, '');
+    const cleanedCvc = this.cvc.replace(/\s/g, '');
+  
+    // Razdvojite uneti datum u formatu "MM/YY" u godinu i mesec
+    const [inputMonth, inputYear] = this.formatExpirationDate().split('/').map(Number);
+  
+    // Dobavite trenutni datum
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear() % 100; // Dobijanje poslednje dve cifre trenutne godine
+    const currentMonth = currentDate.getMonth() + 1; // Januar je 0, zato dodajemo 1
+  
+    // Proverite da li je broj kartice različit od "4242424242424242"
+    if (cleanedCardNumber !== '4242424242424242') {
+     this.dialogRef.open(NotificationPopupComponent,{
+      data:{text:"You don't have sufficient funds in your account."}
+     })
+      return;
+    }
+  
+    // Proverite da li je datum isteka prošao
+    if (inputYear < currentYear || (inputYear === currentYear && inputMonth < currentMonth)) {
+      this.dialogRef.open(NotificationPopupComponent,{
+        data:{text:"The date has expired"}
+       })
+    } else {
+      
+       
+        this.userService.changeRole();
+        this.dialogRef.open(NotificationPopupComponent,{
+          data:{title:"You are premium now!",text:"Please login again!"}
+         })
+      
+      // Ovde možete izvršiti logiku za uspešno plaćanje
     }
   }
+  
+  // Metod za formatiranje unetog datuma u "MM/YY"
+  formatExpirationDate(): string {
+    // Uzmite unete vrednosti za mesec i godinu
+    const month = this.mounthNumber.trim();
+    const year = this.yearNumber.trim();
+  
+    // Proverite da li su uneti mesec i godina u validnom formatu
+    if (/^\d{1,2}$/.test(month) && /^\d{2}$/.test(year)) {
+      return `${month}/${year}`;
+    } else {
+      console.log('Neispravan format datuma');
+      return '';
+    }
+  }
+  
 }
--- Frontend/src/app/services/user.service.ts
+++ Frontend/src/app/services/user.service.ts
@@ -4,12 +4,15 @@ import { HttpClient,HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
 import { environment } from 'src/environments/environment';
+import { Store } from '@ngrx/store';
+import { selectAuthToken } from '../store/auth/auth.selector';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor(private http:HttpClient) { 
+  constructor(private http:HttpClient,private store:Store) { 
   }
+  authToken$ = this.store.select(selectAuthToken);
   public login(username: string, password: string): Observable<{ access_token: string }> {
     const body = {
       username: username,
@@ -43,6 +46,19 @@ export class UserService {
   getProfileImage(imageName:string) {
     const requestOptions: Object = { responseType: 'blob' };
     return this.http.get<Blob>(`${environment.api}user/profile-image/${imageName}`, requestOptions);
+  }
+  changeRole()
+  {
+    console.log('a');
+    let jwtToken :any;
+    this.authToken$.subscribe((authToken) => {
+      jwtToken = authToken?.access_token;
+    });    
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${jwtToken}`,
+    });
+    console.log(jwtToken);
+    this.http.put(environment.api + "user/updateUserRole",{},{ headers }).subscribe((response)=>console.log(response));
   }
   
 }
