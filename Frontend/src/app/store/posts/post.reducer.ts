8d5853b9d2b71fbfa4b74bf738ba1ece14bf206e
VVeljovic <veljkoveljovic13@gmail.com>
2023-09-30 13:57:47 +0200

    ngrx-entities

--- Frontend/src/app/store/posts/post.reducer.ts
+++ Frontend/src/app/store/posts/post.reducer.ts
@@ -1,73 +1,37 @@
 import { createReducer, on } from "@ngrx/store";
+import { EntityState, EntityAdapter, createEntityAdapter } from "@ngrx/entity";
 import * as fromPostActions from "./post.actions";
 import { Post } from "src/app/models/post.model";
-export interface PostState {
-  posts: Post[];
+
+// Definišite entitetni adapter za postove
+export const postAdapter: EntityAdapter<Post> = createEntityAdapter<Post>();
+
+// Definišite interfejs za PostState sa EntityState<Post>
+export interface PostState extends EntityState<Post> {
   postImages: { [imageName: string]: Blob };
   loading: boolean;
   error: any;
 }
 
-export const initialState: PostState = {
-  posts: [],
+export const initialState: PostState = postAdapter.getInitialState({
   postImages: {},
   loading: false,
   error: null,
-};
+});
 
 export const postReducer = createReducer(
   initialState,
   on(fromPostActions.loadPosts, (state) => ({ ...state, loading: true })),
-  on(fromPostActions.loadPostsSuccess, (state, { posts }) => ({
-    ...state,
-    posts,
-    loading: false,
-    error: null,
-  })),
-  on(fromPostActions.loadPostsFailure, (state, { error }) => ({
-    ...state,
-    loading: false,
-    error,
-  })),
-  on(fromPostActions.createPost, (state, { post }) => ({
-    ...state,
-    posts: [...state.posts,  post ],
-  })),
-  on(fromPostActions.createPostFailure, (state, { error }) => ({
-    ...state,
-    error
-  })),
-  on(fromPostActions.loadPostImageSuccess, (state, { imageName, imageBlob }) => ({
-    ...state,
-    postImages: { ...state.postImages, [imageName]: imageBlob },
-  })),
+  on(fromPostActions.loadPostsSuccess, (state, { posts }) => postAdapter.setAll(posts, { ...state, loading: false, error: null })),
+  on(fromPostActions.loadPostsFailure, (state, { error }) => ({ ...state, loading: false, error })),
+  on(fromPostActions.createPost, (state, { post }) => postAdapter.addOne(post, { ...state })),
+  on(fromPostActions.createPostSuccess, (state) => ({ ...state, loading: false, error: null })),
+  on(fromPostActions.createPostFailure, (state, { error }) => ({ ...state, error })),
+  on(fromPostActions.loadPostImageSuccess, (state, { imageName, imageBlob }) => ({ ...state, postImages: { ...state.postImages, [imageName]: imageBlob } })),
   on(fromPostActions.loadFilteredPosts, (state) => ({ ...state, loading: true })),
-  on(fromPostActions.loadFilteredPostsSuccess, (state, { posts }) => ({
-    ...state,
-    posts,
-    loading: false,
-    error: null,
-  })),
-  
-  on(fromPostActions.loadFilteredPostsFailure, (state, { error }) => ({
-    ...state,
-    loading: false,
-    error,
-  })),
-  on(fromPostActions.uploadPostImage, (state) => ({
-    ...state,
-    loading: true,
-    error: null,
-  })),
-  on(fromPostActions.uploadPostImageSuccess, (state, { imageName }) => ({
-    ...state,
-    loading: false,
-    error: null,
-    postImages: { ...state.postImages},
-  })),
-  on(fromPostActions.uploadPostImageFailure, (state, { error }) => ({
-    ...state,
-    loading: false,
-    error,
-  })),
+  on(fromPostActions.loadFilteredPostsSuccess, (state, { posts }) => postAdapter.setAll(posts, { ...state, loading: false, error: null })),
+  on(fromPostActions.loadFilteredPostsFailure, (state, { error }) => ({ ...state, loading: false, error })),
+  on(fromPostActions.uploadPostImage, (state) => ({ ...state, loading: true, error: null })),
+  on(fromPostActions.uploadPostImageSuccess, (state, { imageName }) => ({ ...state, loading: false, error: null })),
+  on(fromPostActions.uploadPostImageFailure, (state, { error }) => ({ ...state, loading: false, error })),
 );
--- Frontend/src/app/store/posts/post.selector.ts
+++ Frontend/src/app/store/posts/post.selector.ts
@@ -1,15 +1,18 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { PostState } from './post.reducer';
-
+import { Post } from 'src/app/models/post.model';
 // Kreirajte selektor za deo stanja 'posts'
 export const selectPostState = createFeatureSelector<PostState>('posts');
 
 // Kreirajte selektor za niz 'posts'
 export const selectPosts = createSelector(
   selectPostState,
-  (state: PostState) => state.posts
+  (state: PostState) => state.ids.map((id)=>state.entities[id]).filter((state)=>state!=null).map((state)=><Post>state)
+  // Koristi selectAll umesto state.posts
 );
 export const selectPostImages = createSelector(
   selectPostState,
   (state: PostState) => state.postImages
 );
+
+
